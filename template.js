2ae3c2be0741cd82c5021ce65fa00b7071e6811c
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-01 15:08:35 +0600

    template

---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "justing";
+const lastName = "timberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+
+console.log(fullName);
+console.log(fullName2);
+
+const multiline = "I am shahriar jalal\n" + "I am 25 Years old\n" + "I love programming";
+const multiline2 = `I am shahriar Jalal\n I am 25 Years old\n I love programming`;
+
+console.log(multiline);
+console.log(multiline2);
